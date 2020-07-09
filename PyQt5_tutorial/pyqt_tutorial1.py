06f82639ee683b3183c17467e45baeef86d6ff0b
teang1995 <teang19951@gmail.com>
2020-07-09 20:39:43 +0900

    added pyscripts

---
+++ PyQt5_tutorial/pyqt_tutorial1.py
@@ -0,0 +1,21 @@
+# EX 3 - 1. 창 띄우기
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("My first Application")
+        self.move(300, 300)
+        self.resize(400, 400)
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
